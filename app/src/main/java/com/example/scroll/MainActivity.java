eec886ed8f3e53f7a691fdc2a00f359abc1ad9e2
annkor07 <korecka_anna@ckziu2.edu.pl>
2026-04-06 01:05:02 +0200

    Dodawanie plików

--- app/src/main/java/com/example/scroll/MainActivity.java
+++ app/src/main/java/com/example/scroll/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.scroll;
 
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -23,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     Button btnFiles;
     Button btnColorMode;
     Button btnGoUp;
+    LinearLayout container;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
         btnColorMode = findViewById(R.id.btnColorMode);
         btnGoUp = findViewById(R.id.goUp);
         etTempo = findViewById(R.id.etTempo);
+        container = findViewById(R.id.container);
 
         btnGoUp.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 if (!isRunning){
                     isRunning = true;
-                    btnStartStop.setText("Stop");
+                    btnStartStop.setText("⏹");
                     String tempo = etTempo.getText().toString();
                     if (!tempo.isEmpty()) {
                         speed = Integer.parseInt(tempo);
@@ -72,18 +82,49 @@ public class MainActivity extends AppCompatActivity {
                                 handler.postDelayed(this, 16);
                                 if (!scrollView.canScrollVertically(1)) {
                                     isRunning = false;
-                                    btnStartStop.setText("Start");
+                                    btnStartStop.setText("►");
                                 }
                             }
                         }
                     });
                 }else{
                     isRunning = false;
+                    btnStartStop.setText("►");
                 }
 
             }
+
+
         });
+        ActivityResultLauncher<String> pickImagesLauncher =
+                registerForActivityResult(new ActivityResultContracts.GetMultipleContents(),
+                        uris -> {
+                            if (uris != null && !uris.isEmpty()) {
+                                for (Uri uri : uris) {
+                                    addImageToLayout(uri);
+                                }
+                            }
+                        }
+                );
+
+        btnFiles.setOnClickListener(v -> pickImagesLauncher.launch("image/*"));
     }
 
+    private void addImageToLayout(Uri uri) {
+        ImageView imageView = new ImageView(this);
 
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT // teraz dostosowuje wysokość proporcjonalnie
+        );
+        params.setMargins(0, 20, 0, 20);
+        imageView.setLayoutParams(params);
+
+        imageView.setScaleType(ImageView.ScaleType.FIT_CENTER); // całe zdjęcie widać
+        imageView.setAdjustViewBounds(true); // zachowuje proporcje
+        imageView.setImageURI(uri);
+
+        container.addView(imageView);
+        scrollView.post(() -> scrollView.fullScroll(View.FOCUS_DOWN));
+    }
 }
